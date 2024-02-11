8c65a070178a8b8dba598938421bc9457f9d9237
lius <edmondLau1018@foxmail.com>
2024-02-11 13:01:05 +0800

    新增对应的 store 和 router

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css';
+import store from './store'
+import router from './router'
 
-createApp(App).use(Antd).mount('#app')
+createApp(App).use(router).use(store).use(Antd).mount('#app')
