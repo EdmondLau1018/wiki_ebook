0482d98ea0957b68cc9ebf3d2b9dc2d66cdaf11a
lius <edmondLau1018@foxmail.com>
2024-02-11 08:53:08 +0800

    写一个测试接口

--- src/main/java/com/edmond/wiki/WikiApplication.java
+++ src/main/java/com/edmond/wiki/WikiApplication.java
@@ -1,11 +1,21 @@
 package com.edmond.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.edmond"})  //  扫描多个目录
 public class WikiApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
+        LOG.info("**********************项目启动成功**********************");
+        LOG.info("当前项目运行在：http://127.0.0.1:8880 端口" );
+        LOG.info("**********************项目启动成功**********************");
+
     }
 }
---
+++ src/main/java/com/edmond/wiki/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.edmond.wiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping(value = "/demo",method = RequestMethod.GET)
+    public String getDemo(){
+        return "demo fucking world ";
+    }
+}
